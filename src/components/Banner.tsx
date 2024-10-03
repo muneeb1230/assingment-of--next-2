8ecfad40f664c21e7d7250ef7ac471e90bc5f4c4
Muneeb Malik <144794302+muneeb1230@users.noreply.github.com>
2024-10-03 15:35:52 +0500

    This is my first project with next

---
+++ src/components/Banner.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='section'>
+        <div className="row">
+            <div className="box">
+            <p>Hi it's </p>
+            <h1 className=''>Kamran Tessori ali</h1>
+            <p>Kamran Tessori (Urdu: کامران ٹیسوری) is a Pakistani politician and businessman, currently serving as the Governor of Sindh[3] since 9 October 2022.[4] Tessori is a member of the Muttahida Qaumi Movement – Pakistan party..</p>     
+            </div>
+
+        <div className="img">
+            <img src="/kamran tessori.jpeg" alt="" />
+        </div>
+
+        </div>
+       
+
+    </div>
+  )
+}
+
+export default Banner
